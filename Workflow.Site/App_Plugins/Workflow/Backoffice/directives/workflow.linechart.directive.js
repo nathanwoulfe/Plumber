2f5f08d65b9ff4a41513abd6731d183ffad7a962
Nathan Woulfe <nathan@nathanw.com.au>
2017-03-13 16:24:57 +1000

    start charting on admin dash

---
+++ Workflow.Site/App_Plugins/Workflow/Backoffice/directives/workflow.linechart.directive.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    function lineChartDirective() {
+
+        var directive = {
+            restrict: 'E',
+            templateUrl: '../app_plugins/workflow/backoffice/partials/workflowLineChart.html',
+            scope: {
+                options: '='
+            },
+            link: function(scope, element) {
+                Highcharts.chart(element[0], scope.options);
+            }
+        };
+
+        return directive;
+    }
+
+    angular.module('umbraco.directives').directive('wfLineChart', lineChartDirective);
+
+}());
