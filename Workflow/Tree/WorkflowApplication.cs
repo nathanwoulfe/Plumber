f393257d93e7e2fcffdfc50b0a4dda6a50f394d0
Nathan Woulfe <nathan@nathanw.com.au>
2017-04-16 14:45:18 +1000

    section order in left-menu - set to 10

--- Workflow/Tree/WorkflowApplication.cs
+++ Workflow/Tree/WorkflowApplication.cs
@@ -3,7 +3,7 @@ using umbraco.interfaces;
 
 namespace Workflow.Actions
 {
-    [Application("workflow", "Workflow", "icon-path")]
+    [Application("workflow", "Workflow", "icon-path", 10)]
     public class WorkflowApplication : IApplication
     {
     }
