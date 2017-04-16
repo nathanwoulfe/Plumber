dcf6c1ff41b6c2642840771c39b2d4c3fa9969c4
Nathan Woulfe <nathan@nathanw.com.au>
2017-04-16 14:13:03 +1000

    restore missing linebreak

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -3,7 +3,8 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 ECHO PACKAGE_SUFFIX : %UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
-cd ..\Workflow\App_Plugins\WorkflowCall npm install
+cd ..\Workflow\App_Plugins\Workflow
+Call npm install
 Call grunt default --buildversion=%APPVEYOR_BUILD_VERSION% --buildbranch=%APPVEYOR_REPO_BRANCH% --packagesuffix=%UMBRACO_PACKAGE_PRERELEASE_SUFFIX%
 cd ..\..\..\BuildPackage\
 Call Tools\nuget.exe restore ..\Workflow.sln
