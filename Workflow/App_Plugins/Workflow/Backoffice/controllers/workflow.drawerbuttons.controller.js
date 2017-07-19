cdd8717b2cce8c0c8698d3594e2d64a5827c4822
Nathan Woulfe <nathan@nathanw.com.au>
2017-07-19 21:09:12 +1000

    include save and publish if current user permissions allow

--- Workflow/App_Plugins/Workflow/Backoffice/controllers/workflow.drawerbuttons.controller.js
+++ Workflow/App_Plugins/Workflow/Backoffice/controllers/workflow.drawerbuttons.controller.js
@@ -2,53 +2,68 @@
     'use strict';
 
     // create controller 
-    function controller($scope, userService, workflowResource, workflowActionsService, contentEditingHelper, contentResource, editorState) {
+    function controller($scope, userService, workflowResource, workflowActionsService, contentEditingHelper, contentResource, editorState, $routeParams) {
         var vm = this;
 
-        workflowResource.getNodePendingTasks(editorState.current.id)
-            .then(function (resp) {
-                if (resp.items.length) {
-                    vm.active = true;
-                    checkUserAccess(resp.items[0]);
-                }
-                else {
-                    setButtons();
-                }
-            }, function (err) {
+        var defaultButtons = contentEditingHelper.configureContentEditorButtons({
+            create: $routeParams.create,
+            content: $scope.content,
+            methods: {
+                saveAndPublish: $scope.saveAndPublish,
+                sendToPublish: $scope.sendToPublish,
+                save: $scope.save,
+                unPublish: angular.noop
+            }
+        });
 
-            });
+        var saveAndPublish = defaultButtons.defaultButton.labelKey === 'buttons_saveAndPublish';
+
+        function getNodeTasks() {
+            workflowResource.getNodePendingTasks(editorState.current.id)
+                .then(function (resp) {
+                    if (resp.items.length) {
+                        vm.active = true;
+                        checkUserAccess(resp.items[0]);
+                    }
+                    else {
+                        setButtons();
+                    }
+                }, function (err) {
+
+                });
+        }
 
         // must be a better way of doing this - need to watch the editor state to dynamically change buttons
         $scope.$watch('$parent.$parent.$parent.contentForm.$dirty', function (newVal, oldVal) {
-            $scope.dirty = newVal === true;           
+            $scope.dirty = newVal === true;
             setButtons();
         });
 
         function setButtons() {
+            
+            var subButtons = saveAndPublish ? [buttons.unpublishButton, defaultButtons.defaultButton, buttons.saveButton] : [buttons.unpublishButton, buttons.saveButton];
+
             vm.buttonGroup = {
                 defaultButton: $scope.dirty ? buttons.saveButton : buttons.publishButton,
-                subButtons: $scope.dirty ? [] : [buttons.unpublishButton, buttons.saveButton]
+                subButtons: $scope.dirty ? (saveAndPublish ? [defaultButtons.defaultButton] : []) : subButtons
             };
         }
 
         function checkUserAccess(task) {
-            userService.getCurrentUser()
-                .then(function (user) {
-                    vm.task = task;
-                    vm.adminUser = user.allowedSections.indexOf('workflow') !== -1;
-                    var currentTaskUsers = task.permissions[task.currentStep].userGroup.usersSummary;
+            vm.task = task;
+            vm.adminUser = user.allowedSections.indexOf('workflow') !== -1;
+            var currentTaskUsers = task.permissions[task.currentStep].userGroup.usersSummary;
 
-                    if (currentTaskUsers.indexOf('|' + user.id + '|') !== -1) {
-                        vm.canAction = true;
-                    }
+            if (currentTaskUsers.indexOf('|' + user.id + '|') !== -1) {
+                vm.canAction = true;
+            }
 
-                    if (vm.active) {
-                        vm.buttonGroup = {
-                            defaultButton: vm.adminUser || vm.canAction ? buttons.cancelButton : buttons.approveButton,
-                            subButtons: vm.adminUser || vm.canAction ? [] : [buttons.rejectButton, buttons.cancelButton]
-                        };
-                    }
-                });
+            if (vm.active) {
+                vm.buttonGroup = {
+                    defaultButton: vm.adminUser || vm.canAction ? buttons.cancelButton : buttons.approveButton,
+                    subButtons: vm.adminUser || vm.canAction ? [] : [buttons.rejectButton, buttons.cancelButton]
+                };
+            }
         }
 
         var buttons = {
@@ -98,11 +113,18 @@
                 handler: function (item) {
                     vm.workflowOverlay = workflowActionsService.initiate(editorState.current.name, editorState.current.id, false);
                 }
-            }        
+            }
         };
 
+        var user;
+        userService.getCurrentUser()
+            .then(function (user) {
+                user = user;
+                getNodeTasks();
+            });
+
         angular.extend(vm, {
-            active: false           
+            active: false
         });
     }
 
