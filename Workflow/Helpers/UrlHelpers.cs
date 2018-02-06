d5d3fb09e1509d9bee5f4f901f0d99f575209ea7
nwoulfe1 <nathan@nathanw.com.au>
2018-02-07 09:23:24 +1000

    Make code smell less

--- Workflow/Helpers/UrlHelpers.cs
+++ Workflow/Helpers/UrlHelpers.cs
@@ -5,8 +5,7 @@ namespace Workflow.Helpers
 {
     public class UrlHelpers
     {
-        public const string ContentEditUrlFormat = "/umbraco#/content/content/edit/{0}";
-        public const string ContentFrameUrlFormat = "/umbraco/backoffice/UmbracoApi/Content/GetById?id={0}";
+        private const string ContentEditUrlFormat = "/umbraco#/content/content/edit/{0}";
 
         /// <summary>
         /// This method gives a fully qualified url and can be used without an HTTPContext 
@@ -29,13 +28,23 @@ namespace Workflow.Helpers
             var baseUrl = new Uri(editUrl);
             return (new Uri(baseUrl, partialUrl)).ToString();
         }
+        
+        /// <summary>
+        /// Gets the fully qualified url needed to open the back office content editor pane for the document with the given Id
+        /// </summary>
+        /// <param name="docId"></param>
+        /// <returns></returns>
+        public static string GetFullyQualifiedContentEditorUrl(int docId)
+        {
+            return GetFullyQualifiedEditUrl(string.Format(ContentEditUrlFormat, docId));
+        }
 
         /// <summary>
         /// This method gives a fully qualified Back Office Edit url
         /// </summary>
         /// <param name="partialUrl">The partial url to fully qualify</param>
         /// <returns>The fully qualified Back Office edit url</returns>
-        public static string GetFullyQualifiedEditUrl(string partialUrl)
+        private static string GetFullyQualifiedEditUrl(string partialUrl)
         {
             var editUrl = Utility.GetSettings().EditUrl;
             if (string.IsNullOrEmpty(editUrl))
@@ -50,12 +59,6 @@ namespace Workflow.Helpers
 
             var baseUrl = new Uri(editUrl.StartsWith("http") ? editUrl : $"http://{editUrl}");
             return (new Uri(baseUrl, partialUrl)).ToString();
-        }
-
-        // Gets the fully qualified url needed to open the back office content editor pane for the document with the given Id
-        public static string GetFullyQualifiedContentEditorUrl(int docId)
-        {
-            return GetFullyQualifiedEditUrl(string.Format(ContentEditUrlFormat, docId));
         }
     }
 }
