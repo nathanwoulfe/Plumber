5b4cd31bb9644cb8a23489c2e6582a5d348561b6
nwoulfe1 <nathan@nathanw.com.au>
2018-02-14 20:22:48 +1000

    add enum extension for checking task/instance status

---
+++ Workflow/Extensions/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Workflow.Models;
+
+namespace Workflow.Extensions
+{
+    internal static class EnumExtensions
+    {
+        /// <summary>
+        /// Is the task status included in the provided arguments set
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="set"></param>
+        /// <returns></returns>
+        public static bool In(this TaskStatus? status, params object[] set)
+        {
+            return set.Contains(status);
+        }
+
+        /// <summary>
+        /// Is the task status NOT included in the provided arguments set
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="set"></param>
+        /// <returns></returns>
+        public static bool NotIn(this TaskStatus? status, params object[] set)
+        {
+            return !set.Contains(status);
+        }
+    }
+}
