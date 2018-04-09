9e16ceee876c726b4cdcef6a6f8f8d7391cf7443
nwoulfe1 <nathan@nathanw.com.au>
2018-04-09 20:05:00 +1000

    es6 time kids

--- Workflow/App_Plugins/Workflow/Backoffice/controllers/workflow.cancel.controller.js
+++ Workflow/App_Plugins/Workflow/Backoffice/controllers/workflow.cancel.controller.js
@@ -4,9 +4,10 @@
     // create controller 
     function cancelController($scope) {
         $scope.model.comment = '';
-        $scope.limit = 250;
-        $scope.intro = 'This operation will cancel the workflow on this document and notify the workflow participants. Are you sure?';
-        $scope.disabled = $scope.model.isFinalApproval === true ? false : true;
+        this.limit = 250;
+        this.intro = 'This operation will cancel the workflow on this document and notify the workflow participants. Are you sure?';
+        this.disabled = $scope.model.isFinalApproval === true ? false : true;
+        debugger;
     }
 
     // register controller 
