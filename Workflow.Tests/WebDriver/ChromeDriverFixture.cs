2de6b7993c08a1688e8590a879e8af6d0a0a64db
nwoulfe1 <nathan@nathanw.com.au>
2018-05-11 18:37:04 +1000

    selenium test stuff

---
+++ Workflow.Tests/WebDriver/ChromeDriverFixture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Workflow.Tests.WebDriver
+{
+    public class ChromeDriverFixture : IDisposable
+    {
+        public readonly ChromeDriver Driver;
+
+        private const string EditorUser = "EditorUser@mail.com";
+        private const string AdminUser = "AdminUser@mail.com";
+        private const string EditorPassword = "JOP{H#kG";
+        private const string AdminPassword = "tzX)TSiA";
+
+        public ChromeDriverFixture()
+        {
+            Driver = new ChromeDriver
+            {
+                Url = "http://localhost:56565/umbraco"
+            };
+        }
+
+        private void Login(string user, string pass)
+        {
+            Driver.FindElementByName("username").SendKeys(user);
+            Driver.FindElementByName("password").SendKeys(pass);
+
+            Driver.FindElementByTagName("button").Click();
+
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(2));
+            wait.Until(ExpectedConditions.ElementExists(By.TagName("section")));
+        }
+
+        public void EditorLogin()
+        {
+            Login(EditorUser, EditorPassword);
+        }
+
+        public void AdminLogin()
+        {
+            Login(AdminUser, AdminPassword);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            Driver.Dispose();
+        }
+    }
+
+    [CollectionDefinition("ChromeDriver collection")]
+    public class ChromeDriverCollection : ICollectionFixture<ChromeDriverFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
---
+++ Workflow.Tests/WebDriver/EditorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Workflow.Tests.WebDriver
+{
+    [Collection("ChromeDriver collection")]
+    public class EditorTests
+    {
+        private readonly ChromeDriver _driver;
+
+        public EditorTests(ChromeDriverFixture fixture)
+        {
+            _driver = fixture.Driver;
+            fixture.EditorLogin();
+        }
+
+        [Fact]
+        public void Editor_Cant_Access_Workflow_Section()
+        {
+            Assert.Throws<NoSuchElementException>(() => _driver.FindElementByCssSelector("[data-element*='section-workflow']"));
+        }
+
+        [Fact]
+        public void Editor_Cant_Save_And_Publish()
+        {
+            _driver.Url = "http://localhost:56565/umbraco#/content/content/edit/1089";
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementExists(By.ClassName("workflow-button-drawer")));
+            Assert.Throws<NoSuchElementException>(() => _driver.FindElementByLinkText("Save and publish"));
+        }
+
+    }
+}
+
