6a89e54035b9a4eb8ac43089e001403aa6d521b6
nwoulfe1 <nathan@nathanw.com.au>
2020-07-22 10:15:34 +1000

    bump version to 1.1.15

--- Workflow/Properties/AssemblyInfo.cs
+++ Workflow/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.11.2.457")]
-[assembly: AssemblyVersion("1.1.14")]
-[assembly: AssemblyFileVersion("1.1.14")]
+[assembly: AssemblyVersion("1.1.15")]
+[assembly: AssemblyFileVersion("1.1.15")]
